aa33a8a1d7c3fd5b1490d284cb2457d42a9e9e2a
Antoine Grondin <antoinegrondin@gmail.com>
2013-11-02 15:34:10 -0400

    Remove cockpit.  Fix keypresses.

--- keyboard.js
+++ keyboard.js
@@ -30,7 +30,7 @@ var doTakeOff = function() {
 // Climb
 // Climb
 var climb = 0.0;
-var climbRate = 0.01;
+var climbRate = 0.03;
 var applyClimb = function(climb) {
     if (climb > 0.0) {
         console.log("Climb="+climb + " -> up");
@@ -57,7 +57,7 @@ var doDown = function() {
 // Pitch
 // Pitch
 var pitch = 0.0;
-var pitchRate = 0.1;
+var pitchRate = 0.03;
 var applyPitch = function(pitch) {
     if (pitch > 0.0) {
         console.log("Pitch="+pitch + " -> front");
@@ -84,7 +84,7 @@ var pitchDown = function() {
 // Yaw
 // Yaw
 var yaw = 0.0;
-var yawRate = 0.01;
+var yawRate = 0.03;
 var applyYaw = function(yaw) {
     if (yaw > 0.0) {
         console.log("Yaw="+yaw + " -> ccw");
@@ -111,7 +111,7 @@ var yawRight = function() {
 // Roll
 // Roll
 var roll = 0.0;
-var rollRate = 0.01;
+var rollRate = 0.03;
 var applyRoll = function(roll) {
     if (roll > 0.0) {
         console.log("Roll="+roll + " -> right");
@@ -188,9 +188,6 @@ var dispatchKey = function(key) {
     case 't':
         stabilize();
         break;
-    case 'm':
-        showVideo();
-        break;
     default:
         process.stdout.write("Not handled: " + key + "\n");
   }
@@ -209,16 +206,116 @@ stdin.on('data', function(key){
 
 app.listen(3000);
 
+var w = 87,
+    s = 83,
+    a = 65,
+    d = 68,
+    q = 81,
+    e = 69,
+    t = 84,
+    shift = 16,
+    command = 91,
+    space = 32;
+
+var isKeyDownNow = {};
+isKeyDownNow[w] = false,
+isKeyDownNow[s] = false,
+isKeyDownNow[a] = false,
+isKeyDownNow[d] = false,
+isKeyDownNow[q] = false,
+isKeyDownNow[e] = false,
+isKeyDownNow[t] = false,
+isKeyDownNow[shift] = false,
+isKeyDownNow[command] = false,
+isKeyDownNow[space] = false;
+
+var wasKeyConsumedOnce = {};
+wasKeyConsumedOnce[w] = true,
+wasKeyConsumedOnce[s] = true,
+wasKeyConsumedOnce[a] = true,
+wasKeyConsumedOnce[d] = true,
+wasKeyConsumedOnce[q] = true,
+wasKeyConsumedOnce[e] = true,
+wasKeyConsumedOnce[t] = true,
+wasKeyConsumedOnce[shift] = true,
+wasKeyConsumedOnce[command] = true,
+wasKeyConsumedOnce[space] = true;
+
 io.sockets.on('connection', function(socket) {
     setInterval(function(){
         var batteryLevel = client.battery();
         socket.emit('event', { name: 'battery',value: batteryLevel});
     },1000);
 
-    socket.on('keyboard_event', function(key) {
-        console.log("Browser sent key=" + key)
+    socket.on('key_down', function(key) {
+        isKeyDownNow[key] = true;
+        wasKeyConsumedOnce[key] = false;
+    });
+
+    socket.on('key_up', function(key) {
+        isKeyDownNow[key] = false;
     });
 });
+
+
+setInterval(function() {
+    if (!wasKeyConsumedOnce[space]) {
+        wasKeyConsumedOnce[space] = true;
+        stabilize();
+        doTakeOff();
+    }
+    if (isKeyDownNow[shift] || !wasKeyConsumedOnce[shift]) {
+        wasKeyConsumedOnce[shift] = true;
+        doUp();
+    }
+    if (isKeyDownNow[command] || !wasKeyConsumedOnce[command]) {
+        wasKeyConsumedOnce[command] = true;
+        doDown();
+    }
+    if (isKeyDownNow[w] || !wasKeyConsumedOnce[w]) {
+        wasKeyConsumedOnce[w] = true;
+        pitchUp();
+    }
+    if (isKeyDownNow[s] || !wasKeyConsumedOnce[s]) {
+        wasKeyConsumedOnce[s] = true;
+        pitchDown();
+    }
+    if (isKeyDownNow[a] || !wasKeyConsumedOnce[a]) {
+        wasKeyConsumedOnce[a] = true;
+        yawLeft();
+    }
+    if (isKeyDownNow[d] || !wasKeyConsumedOnce[d]) {
+        wasKeyConsumedOnce[d] = true;
+        yawRight();
+    }
+    if (isKeyDownNow[q] || !wasKeyConsumedOnce[q]) {
+        wasKeyConsumedOnce[q] = true;
+        rollCW();
+    }
+    if (isKeyDownNow[e] || !wasKeyConsumedOnce[e]) {
+        wasKeyConsumedOnce[e] = true;
+        rollCounterCW();
+    }
+    if (isKeyDownNow[t] || !wasKeyConsumedOnce[t]) {
+        wasKeyConsumedOnce[t] = true;
+
+        isKeyDownNow[w] = false;
+        isKeyDownNow[s] = false;
+        isKeyDownNow[a] = false;
+        isKeyDownNow[d] = false;
+        isKeyDownNow[q] = false;
+        isKeyDownNow[e] = false;
+        isKeyDownNow[t] = false;
+        isKeyDownNow[shift] = false;
+        isKeyDownNow[command] = false;
+        isKeyDownNow[space] = false;
+
+        stabilize();
+    }
+
+
+}, 30);
+
 
 require("dronestream").listen(3001);
 
